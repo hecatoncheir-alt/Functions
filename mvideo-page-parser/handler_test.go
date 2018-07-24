9f2b8dc4c82dd7b8cc6d5feb1304eb5a4f7b4da7
Rasarts <rasart.pro@gmail.com>
2018-07-24 20:49:36 +0300

    Added pageParse method. Update Handler.

--- mvideo-page-parser/handler_test.go
+++ mvideo-page-parser/handler_test.go
@@ -1,1 +1,59 @@
 package function
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestParserCanParsePage(t *testing.T) {
+	testFileContent, err := ioutil.ReadFile("handler_test_page.html")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		_, err := w.Write(testFileContent)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	})
+
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	request := Request{
+		IRI: fmt.Sprint(server.URL, "/test"),
+		Instructions: Instructions{
+			ItemSelector:               ".c-product-tile",
+			PreviewImageOfItemSelector: ".c-product-tile-picture__link .lazy-load-image-holder img",
+			NameOfItemSelector:         ".c-product-tile__description .sel-product-tile-title",
+			LinkOfItemSelector:         ".c-product-tile__description .sel-product-tile-title",
+			PriceOfItemSelector:        ".c-product-tile__checkout-section .c-pdp-price__current"},
+	}
+
+	bytes, err := json.Marshal(request)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	response := Handle(bytes)
+
+	var listOfProducts []Product
+	err = json.Unmarshal([]byte(response), &listOfProducts)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	expectedLengthOfProductsList := 12
+
+	if len(listOfProducts) != expectedLengthOfProductsList {
+		t.Errorf("expected '%d' but got '%d'", expectedLengthOfProductsList, len(listOfProducts))
+	}
+}
