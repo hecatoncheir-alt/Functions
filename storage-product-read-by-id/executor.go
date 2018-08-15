16b6b7a3168787786b8fcf99288494bc3f51d2c5
Rasarts <rasart.pro@gmail.com>
2018-08-15 06:41:45 +0300

    Added ReadCategoriesByID.

---
+++ storage-product-read-by-id/executor.go
@@ -0,0 +1,156 @@
+package function
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/hecatoncheir/Storage"
+	"log"
+	"os"
+	"text/template"
+)
+
+type Storage interface {
+	Query(string) ([]byte, error)
+}
+
+type Executor struct {
+	Store Storage
+}
+
+var logger = log.New(os.Stdout, "Executor: ", log.Lshortfile)
+
+var (
+	// ErrProductCanNotBeWithoutID means that product can't be found in storage for make some operation
+	ErrProductCanNotBeWithoutID = errors.New("product can not be without id")
+
+	// ErrProductByIDCanNotBeFound means that the product can't be found in database
+	ErrProductByIDCanNotBeFound = errors.New("product by id can not be found")
+
+	// ErrProductDoesNotExist means than the product does not exist in database
+	ErrProductDoesNotExist = errors.New("product does not exist")
+)
+
+// ReadProductByID is a method for get all nodes of categories by ID
+func (executor *Executor) ReadProductByID(productID, language string) (storage.Product, error) {
+	product := storage.Product{}
+
+	if productID == "" {
+		logger.Println(ErrProductCanNotBeWithoutID)
+		return product, ErrProductCanNotBeWithoutID
+	}
+
+	variables := struct {
+		ProductID string
+		Language  string
+	}{
+		ProductID: productID,
+		Language:  language}
+
+	queryTemplate, err := template.New("ReadProductByID").Parse(`{
+				products(func: uid("{{.ProductID}}")) @filter(has(productName)) {
+					uid
+					productName: productName@{{.Language}}
+					productIri
+					previewImageLink
+					productIsActive
+					belongs_to_category @filter(eq(categoryIsActive, true)) {
+						uid
+						categoryName: categoryName@{{.Language}}
+						categoryIsActive
+						belongs_to_company @filter(eq(companyIsActive, true)) {
+							uid
+							companyName: companyName@{{.Language}}
+							has_category @filter(eq(categoryIsActive, true)) {
+								uid
+								categoryName: categoryName@{{.Language}}
+								categoryIsActive
+								belong_to_company @filter(eq(companyIsActive, true)){
+									uid
+									companyName: companyName@{{.Language}}
+									companyIsActive
+								}
+							}
+						}
+					}
+					belongs_to_company @filter(eq(companyIsActive, true)) {
+						uid
+						companyName: companyName@{{.Language}}
+						has_category @filter(eq(categoryIsActive, true)) {
+							uid
+							categoryName: categoryName@{{.Language}}
+							categoryIsActive
+							belong_to_company @filter(eq(companyIsActive, true)){
+								uid
+								companyName: companyName@{{.Language}}
+								companyIsActive
+							}
+						}
+					}
+					has_price @filter(eq(priceIsActive, true)) (orderasc: priceDateTime) {
+						uid
+						priceValue
+						priceDateTime
+						priceCity
+						priceIsActive
+						belongs_to_product @filter(eq(productIsActive, true)) {
+							uid
+							productName: productName@{{.Language}}
+							productIri
+							previewImageLink
+							productIsActive
+							has_price @filter(eq(priceIsActive, true)) {
+								uid
+								priceValue
+								priceDateTime
+								priceCity
+								priceIsActive
+							}
+						}
+						belongs_to_city @filter(eq(cityIsActive, true)) {
+							uid
+							cityName: cityName@{{.Language}}
+							cityIsActive
+						}
+					}
+				}
+			}`)
+
+	product.ID = productID
+
+	if err != nil {
+		log.Println(err)
+		return product, ErrProductByIDCanNotBeFound
+	}
+
+	queryBuf := bytes.Buffer{}
+	err = queryTemplate.Execute(&queryBuf, variables)
+	if err != nil {
+		log.Println(err)
+		return product, ErrProductByIDCanNotBeFound
+	}
+
+	response, err := executor.Store.Query(queryBuf.String())
+	if err != nil {
+		log.Println(err)
+		return product, ErrProductByIDCanNotBeFound
+	}
+
+	type productsInStore struct {
+		Products []storage.Product `json:"products"`
+	}
+
+	var foundedProducts productsInStore
+
+	err = json.Unmarshal(response, &foundedProducts)
+	if err != nil {
+		log.Println(err)
+		return product, ErrProductByIDCanNotBeFound
+	}
+
+	if len(foundedProducts.Products) == 0 {
+		return product, ErrProductDoesNotExist
+	}
+
+	return foundedProducts.Products[0], nil
+}
---
+++ storage-product-read-by-id/executor_test.go
@@ -0,0 +1,99 @@
+package function
+
+import (
+	"github.com/hecatoncheir/Storage"
+	"testing"
+)
+
+// ---------------------------------------------------------------------------------------------------------------------
+func TestProductCanBeReadByID(t *testing.T) {
+	IDOfTestedProduct := "0x12"
+
+	executor := Executor{Store: MockStore{}}
+
+	productFromStore, err := executor.ReadProductByID(IDOfTestedProduct, "ru")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	if productFromStore.ID != "0x12" {
+		t.Fatalf("Expected id of product: '0x12', actual: %v", productFromStore.ID)
+	}
+
+	if productFromStore.Name != "Test product" {
+		t.Fatalf("Expected name of product: 'Test product', actual: %v", productFromStore.Name)
+	}
+}
+
+type MockStore struct {
+	storage.Store
+}
+
+func (store MockStore) Query(request string) (response []byte, err error) {
+
+	resp := `
+		{
+		   "products":[
+			  {
+				 "uid":"0x12",
+				 "productName":"Test product",
+				 "productIri":"http://",
+				 "previewImageLink":"http://",
+				 "productIsActive":true,
+				 "belongs_to_company": [],
+				 "belongs_to_category": [],
+				 "has_price": []
+			  }
+		   ]
+		}
+	`
+
+	return []byte(resp), nil
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+func TestProductCanBeReadByIDWithError(t *testing.T) {
+	IDOfTestedCategory := "0x12"
+
+	executor := Executor{Store: ErrorMockStore{}}
+	_, err := executor.ReadProductByID(IDOfTestedCategory, "ru")
+	if err != ErrProductByIDCanNotBeFound {
+		t.Fatalf(err.Error())
+	}
+}
+
+type ErrorMockStore struct {
+	storage.Store
+}
+
+func (store ErrorMockStore) Query(request string) (response []byte, err error) {
+	return []byte(""), nil
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+func TestProductCanBeReadByIDAndItCanBeEmpty(t *testing.T) {
+	IDOfTestedCompany := "0x12"
+
+	executor := Executor{Store: EmptyMockStore{}}
+	_, err := executor.ReadProductByID(IDOfTestedCompany, "ru")
+	if err != ErrProductDoesNotExist {
+		t.Fatalf(err.Error())
+	}
+}
+
+type EmptyMockStore struct {
+	storage.Store
+}
+
+func (store EmptyMockStore) Query(request string) (response []byte, err error) {
+
+	resp := `
+		{
+		   "products":[]
+		}
+	`
+
+	return []byte(resp), nil
+}
