6ef5473e275304278b8d38123348650ae0759d2b
Vitaliy Vostrikov <rasart.pro@gmail.com>
2018-08-22 16:56:04 +0300

    Added ReadPageInstructionByID.

---
+++ storage-page-instruction-read-by-id/handler.go
@@ -0,0 +1,94 @@
+package function
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/hecatoncheir/Storage"
+)
+
+type Request struct {
+	Language          string
+	PageInstructionID string
+	DatabaseGateway   string
+}
+
+type ErrorResponse struct {
+	Error string
+	Data  ErrorData
+}
+
+type ErrorData struct {
+	Error   string
+	Request string
+}
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	request := Request{}
+
+	err := json.Unmarshal(req, &request)
+	if err != nil {
+		warning := fmt.Sprintf(
+			"Unmarshal request error: %v. Error: %v", request, err)
+
+		fmt.Println(warning)
+
+		errorResponse := ErrorResponse{
+			Error: "Unmarshal request error",
+			Data: ErrorData{
+				Request: string(req),
+				Error:   err.Error()}}
+
+		response, err := json.Marshal(errorResponse)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		return string(response)
+	}
+
+	executor := Executor{Store: &storage.Store{DatabaseGateway: request.DatabaseGateway}}
+	PageInstruction, err := executor.ReadPageInstructionByID(request.PageInstructionID, request.Language)
+	if err != nil {
+		warning := fmt.Sprintf(
+			"ReadPageInstructionByID error: %v", err)
+
+		fmt.Println(warning)
+
+		errorResponse := ErrorResponse{
+			Error: "ReadPageInstructionByID error",
+			Data: ErrorData{
+				Request: string(req),
+				Error:   err.Error()}}
+
+		response, err := json.Marshal(errorResponse)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		return string(response)
+	}
+
+	encodedPageInstruction, err := json.Marshal(PageInstruction)
+	if err != nil {
+		warning := fmt.Sprintf(
+			"Unmarshal PageInstruction error: %v. Error: %v", PageInstruction, err)
+
+		fmt.Println(warning)
+
+		errorResponse := ErrorResponse{
+			Error: "Unmarshal PageInstruction error",
+			Data: ErrorData{
+				Request: string(req),
+				Error:   err.Error()}}
+
+		response, err := json.Marshal(errorResponse)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		return string(response)
+	}
+
+	return string(encodedPageInstruction)
+}
