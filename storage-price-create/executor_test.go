0cb35b17c304c24cc3290575be142e13bda34cb4
Vitaliy Vostrikov <rasart.pro@gmail.com>
2018-09-01 10:54:05 +0300

    Update to new Storage API.

--- storage-price-create/executor_test.go
+++ storage-price-create/executor_test.go
@@ -42,12 +42,8 @@ type MockStorage struct {
 	DatabaseGateway string
 }
 
-func (store MockStorage) Mutate(setJson []byte) (uid string, err error) {
+func (store MockStorage) CreateJSON(setJson []byte) (uid string, err error) {
 	return "0x12", nil
-}
-
-func (store MockStorage) SetNQuads(subject, predicate, object string) error {
-	return nil
 }
 
 // --------------------------------------------------------------------------------------------------------
@@ -69,6 +65,6 @@ type ErrorMockStorage struct {
 	DatabaseGateway string
 }
 
-func (store ErrorMockStorage) Mutate(setJson []byte) (uid string, err error) {
+func (store ErrorMockStorage) CreateJSON(setJson []byte) (uid string, err error) {
 	return "", errors.New("")
 }
