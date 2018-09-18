7544bf9f88098e893fed3f65a5cf7e857d9f832f
Rasarts <rasart.pro@gmail.com>
2018-09-18 20:59:17 +0300

    Added Response type.

--- storage-price-delete/handler.go
+++ storage-price-delete/handler.go
@@ -6,24 +6,8 @@ import (
 	"github.com/hecatoncheir/Storage"
 )
 
-type Request struct {
-	PriceID         string
-	DatabaseGateway string
-}
-
-type ErrorResponse struct {
-	Error string
-	Data  ErrorData
-}
-
-type ErrorData struct {
-	Error   string
-	Request string
-}
-
-type NoErrorResponse struct {
-	Error string
-}
+type Request struct{ PriceID, DatabaseGateway string }
+type Response struct{ Message, Data, Error string }
 
 // Handle a serverless request
 func Handle(req []byte) string {
@@ -36,11 +20,7 @@ func Handle(req []byte) string {
 
 		fmt.Println(warning)
 
-		errorResponse := ErrorResponse{
-			Error: "Unmarshal request error",
-			Data: ErrorData{
-				Request: string(req),
-				Error:   err.Error()}}
+		errorResponse := Response{Message: warning, Data: string(req), Error: err.Error()}
 
 		response, err := json.Marshal(errorResponse)
 		if err != nil {
@@ -58,11 +38,7 @@ func Handle(req []byte) string {
 
 		fmt.Println(warning)
 
-		errorResponse := ErrorResponse{
-			Error: "DeleteEntityByID error",
-			Data: ErrorData{
-				Request: string(req),
-				Error:   err.Error()}}
+		errorResponse := Response{Message: warning, Data: string(req), Error: err.Error()}
 
 		response, err := json.Marshal(errorResponse)
 		if err != nil {
@@ -72,12 +48,12 @@ func Handle(req []byte) string {
 		return string(response)
 	}
 
-	noErrorResponse := NoErrorResponse{Error: ""}
+	response := Response{}
 
-	encodedNoErrorResponse, err := json.Marshal(noErrorResponse)
+	encodedResponse, err := json.Marshal(response)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	return string(encodedNoErrorResponse)
+	return string(encodedResponse)
 }
